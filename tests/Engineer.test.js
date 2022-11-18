2af167b5bad33327cf68da50ca5f7da3efdbbee7
Paultcodes <paultayloremail1995@gmail.com>
2022-11-18 14:33:34 -0600

    Tested engineer class. All tests are passing for engineer

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,2 +1,53 @@
 const Engineer = require("../lib/Engineer");
 
+describe("Engineer", () => {
+  describe("create", () => {
+    it("Should create a new engineer", () => {
+      const engineer = new Engineer(
+        "Paul",
+        44,
+        "paultayloremail1995@gmail.com",
+        "https://github.com/",
+        "Paultcodes"
+      );
+
+      expect(engineer.name).toEqual(expect.any(String));
+      expect(engineer.id).toEqual(expect.any(Number));
+      expect(engineer.email).toEqual(expect.any(String));
+      expect(engineer.githubLink).toEqual(expect.any(String));
+      expect(engineer.github).toEqual(expect.any(String));
+    });
+  });
+});
+
+describe("Github", () => {
+  describe("get", () => {
+    it("Should return engineers github", () => {
+      const engineer = new Engineer(
+        "Paul",
+        44,
+        "paultayloremail1995@gmail.com",
+        "https://github.com/",
+        "Paultcodes"
+      );
+
+      expect(engineer.getGithub()).toEqual("Paultcodes");
+    });
+  });
+});
+
+describe("Role", () => {
+  describe("get", () => {
+    it("Should return engineers role", () => {
+      const engineer = new Engineer(
+        "Paul",
+        44,
+        "paultayloremail1995@gmail.com",
+        "https://github.com/",
+        "Paultcodes"
+      );
+
+      expect(engineer.getRole()).toEqual("Engineer");
+    });
+  });
+});
