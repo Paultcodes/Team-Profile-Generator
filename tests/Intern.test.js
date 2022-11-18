3ee1baa2f239a5221fffc4d1f26e8ba6432451e2
Paultcodes <paultayloremail1995@gmail.com>
2022-11-18 16:23:40 -0600

    Tested intern class. All tests are passing for intern

---
+++ tests/Intern.test.js
@@ -0,0 +1,49 @@
+const Intern = require("../lib/Intern");
+
+describe("Intern", () => {
+  describe("create", () => {
+    it("Should create a new intern", () => {
+      const intern = new Intern(
+        "Paul",
+        22,
+        "paultayloremail1995@gmail.com",
+        "Topeka High"
+      );
+
+      expect(intern.name).toEqual("Paul");
+      expect(intern.id).toEqual(22);
+      expect(intern.email).toEqual("paultayloremail1995@gmail.com");
+      expect(intern.school).toEqual("Topeka High");
+    });
+  });
+});
+
+describe("School", () => {
+  describe("get", () => {
+    it("Should return interns school", () => {
+      const intern = new Intern(
+        "Paul",
+        22,
+        "paultayloremail1995@gmail.com",
+        "Topeka High"
+      );
+
+      expect(intern.getSchool()).toEqual("Topeka High");
+    });
+  });
+});
+
+describe("Role", () => {
+  describe("get", () => {
+    it("Should return interns role", () => {
+      const intern = new Intern(
+        "Paul",
+        22,
+        "paultayloremail1995@gmail.com",
+        "Topeka High"
+      );
+
+      expect(intern.getRole()).toEqual("Intern");
+    });
+  });
+});
